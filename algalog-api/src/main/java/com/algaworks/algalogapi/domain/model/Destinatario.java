63959ebc8f63077766311bd81ebdeaa07d1ac1d8
João Pedro <jpedro1580@gmail.com>
2022-07-23 01:18:09 -0300

    Allow complemento to be blank since it can be unused in some cases

--- algalog-api/src/main/java/com/algaworks/algalogapi/domain/model/Destinatario.java
+++ algalog-api/src/main/java/com/algaworks/algalogapi/domain/model/Destinatario.java
@@ -3,31 +3,31 @@ package com.algaworks.algalogapi.domain.model;
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
 import javax.validation.constraints.NotBlank;
-
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-@Embeddable // Informa que os dados dessa classe pode ser embutida 
-						//em outra classe no banco de dao
+@Embeddable // Informa que os dados dessa classe pode ser embutida
+// em outra classe no banco de dao
 public class Destinatario {
 
-	@NotBlank
-	@Column(name = "destinatario_nome")
-	private String nome;
-	@NotBlank
-	@Column(name = "destinatario_logradouro")
-	private String logradouro;
-	@NotBlank
-	@Column(name = "destinatario_numero")
-	private String numero;
-	@NotBlank
-	@Column(name = "destinatario_complemento")
-	private String complemento;
-	@NotBlank
-	@Column(name = "destinatario_bairro")
-	private String bairro;
+  @NotBlank
+  @Column(name = "destinatario_nome")
+  private String nome;
 
+  @NotBlank
+  @Column(name = "destinatario_logradouro")
+  private String logradouro;
+
+  @NotBlank
+  @Column(name = "destinatario_numero")
+  private String numero;
+
+  @NotBlank
+  @Column(name = "destinatario_bairro")
+  private String bairro;
+
+  @Column(name = "destinatario_complemento")
+  private String complemento;
 }
-
